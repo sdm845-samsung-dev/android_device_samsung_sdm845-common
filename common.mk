53a849d21cb3fd59adf669a78d58fa56c7aafa11
Alessandro Astone <ales.astone@gmail.com>
2020-10-29 08:40:54 +0100

    sdm845-common: Define a soong namespace
    Change-Id: Ida759ee1786c667e766da29103f0ecb54950c104

--- common.mk
+++ common.mk
@@ -29,6 +29,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml \
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH) \
+
 # VNDK
 PRODUCT_EXTRA_VNDK_VERSIONS := \
     28 \
