e1ee61478dd6e72e5460879b4656a2a0348886fa
Iascker <mezackisilver@gmail.com>
2023-09-20 20:25:55 -0300

    sdm845-common: Align the current device tree structure

--- common.mk
+++ common.mk
@@ -6,40 +6,40 @@
 
 # Doze
 PRODUCT_PACKAGES += \
-    SamsungDoze \
+    SamsungDoze
 
 # GSI specific tasks on boot
 PRODUCT_PACKAGES += \
     gsi_skip_mount.cfg \
-    init.gsi.rc \
+    init.gsi.rc
 
 # Init
 PRODUCT_PACKAGES += \
     init.recovery.qcom.rc \
     init.samsung.display.rc \
-    init.samsung.eif.rc \
+    init.samsung.eif.rc
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.bluetooth_le.xml \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH) \
+    $(LOCAL_PATH)
 
 # VNDK
 PRODUCT_EXTRA_VNDK_VERSIONS := \
     28 \
     29 \
     30 \
-    31 \
+    31
 
 # WiFi
 PRODUCT_PACKAGES += \
-    WifiOverlay \
+    WifiOverlay
